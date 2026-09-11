f67ec3bffdd9ad4d4b3e041d47ffc0d887359dc8
Marie Jana Arillo <janaarillo@Maries-MacBook-Pro.local>
2026-09-11 19:09:41 +0800

    fix(mobile): connect web debug directly to localhost backend for sandbox testing

--- mobile/lib/services/api_service.dart
+++ mobile/lib/services/api_service.dart
@@ -1,10 +1,17 @@
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart' as http_parser;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiService {
-  static String get baseUrl => 'https://relieflink-4a13cb419236.herokuapp.com/api';
+  static String get baseUrl {
+    if (kIsWeb && kDebugMode) {
+      return 'http://localhost:5001/api';
+    }
+    return 'https://relieflink-4a13cb419236.herokuapp.com/api';
+  }
+
 
   Map<String, dynamic> _parseResponse(http.Response response) {
     try {
